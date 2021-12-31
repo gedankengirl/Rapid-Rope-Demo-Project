702185036655baf1e7a7d776c07b61381a676f4f
Andrew Zhilin <andrew_zhilin@yahoo.com>
2021-12-31 16:56:02 +0300

    Update RapidRope_README.lua

--- Data/Scripts/RapidRope_README.lua
+++ Data/Scripts/RapidRope_README.lua
@@ -1,5 +1,10 @@
 --[==[
+
 *** RapidRope Component for Core Engine ***
+
+Author: Rinoa (https://www.coregames.com/user/1fe0c4ed2ab2416dba17f5d29ee420a4)
+
+# Introduction
 
 RapidRope is a simple Core component that lets you create real-time client-side
 catenary*-shaped static and dynamic ropes with no coding skills required.
@@ -9,16 +14,30 @@ It also provides a simple optional physically based procedural animator.
 
 It is pretty cheap (for a dynamic rope): one instance of a 30-segment rope
 has the client CPU cost of about 0.3 ms. Ropes that are for the most part
-static (don't have `Move` property enabled and their ends change position
-only occasionally) have a negligible CPU cost.
+static (don't have `EnableMovement` property on and their ends change
+position only occasionally) have a negligible CPU cost.
 
-TODO: перечислить фичи из техзадания.
+# Usage
 
-RapidRope is easy to use. Just drag `RapidRopeTemplate` to your project’s hierarchy, deinstance it and fill in the customs properties of RapidRope.lua script - `StartObject` and `EndObject` - with references to the objects that will serve as a start and end points of your rope. All other custom properties have reasonable default values, but feel free to experiment with them.
+RapidRope is easy to use. Just drag `RapidRopeTemplate` to your project’s
+hierarchy, deinstance it and fill in the customs properties of
+RapidRope.lua script - `StartObject` and `EndObject` - with references to
+the objects that will serve as a start and end points of your rope. All
+other custom properties have reasonable default values, but feel free to
+experiment with them.
 
-By default, the rope is dynamic – it monitors the change in the coordinates of end points and accordingly re-draws itself with a different curve.
-When the end points don’t move, the rope becomes static – i.e., basically free in terms of performance: doing no recalculations, just monitoring the coordinates changes.
-The rope has length. But if you set the end points farther apart than the length allows, the rope will extend, keeping the minimal, barely noticeable curve. If the distance between end points is less than the rope’s length, the curve will be bigger, but the length of the curve (called catenary) will always be equal to the length of the rope.
+By default, the rope is dynamic – it monitors the change in the coordinates
+of end points and accordingly re-draws itself with a different curve.
+
+When the end points don’t move, the rope becomes static – i.e., basically
+free in terms of performance: doing no recalculations, just monitoring the
+coordinates changes.
+
+The rope has length. But if you set the end points farther apart than the
+length allows, the rope will extend, keeping the minimal, barely noticeable
+curve. If the distance between end points is less than the rope’s length,
+the curve will be deeper, but the length of the curve (called catenary)
+will always be equal to the length of the rope.
 
 # Custom Properties:
 
@@ -52,15 +71,35 @@ The rope has length. But if you set the end points farther apart than the length
 - `CustomColor`(Color): if `CustomizeColor` is ticked on, this value will replace default
   color of the `SegmentTemplate`.
 
-- SegmentCount (default: 30): the number of `SegmentTemplate` instances. The
+- `SegmentCount` (default: 30): the number of `SegmentTemplate` instances. The
   client-side runtime CPU cost of dynamic rope is proportional to this
   number (~0.01 ms/segment). For static ropes you can safely use a lot more than dynamic ones.
 
-- [dynamic] SegmentLengthFactor (1.01: default): to change the segment's
+- [dynamic] `SegmentLengthFactor` (1.01: default): to change the segment's
   length. Tweak it to prevent visible banding or visually separate segments
   from each other in some other way, especially if your segment template is not 1m in z-axis.
 
-- [dynamic] ConeFactor (1: default): tweak it to change rope thickness at
+- [dynamic] `ConeFactor` (1: default): tweak it to change rope thickness at
   the end point.
+
+  ## [End Object Animation] section
+
+- [dynamic] `EnableMovement` (default: off):
+
+- [dynamic] `RopeStiffness` (positive number):
+
+- [dynamic] `LinearDamping` (positive number):
+
+- `MaxStretchFactor` (default: 1.1)
+
+- [dynamic] `OutsideForce` (Vector3): -z: gravity; +z lift; x,y - wind.
+
+- [dynamic] `DisableAngularMovement` (on/off):
+
+- [dynamic] `AngularDamping` (positive number):
+
+- [dynamic] `AngularInertia` (positive number):
+
+- [dynamic] `DisableAngularMovement` (default: off):
 
 ]==]
